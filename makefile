4c0dc15fa6e9b639b3e7c549840c10e5ae9c7fe1
Volker Schmid <volker.schmid@regify.com>
2022-08-19 11:33:58 +0200

    Enhanced protocol usage on initial loading.

--- makefile
+++ makefile
@@ -2,4 +2,4 @@ xpi: clean
 	zip -x /*.code-workspace  makefile information.txt README.adoc -r phoneToLinkAddOn.xpi *
 
 clean:
-	rm -f phoneToLinkAddOn.xpi.xpi
+	rm -f phoneToLinkAddOn.xpi
--- phoneToLink.js
+++ phoneToLink.js
@@ -3,10 +3,12 @@ async function makeLinks() {
     // get protocol to use
     let setting = await browser.storage.sync.get("protocol");
 
-    let proto = "callto:";
-    if (setting) {
-        proto = setting.protocol;
-    } 
+    let proto = setting.protocol;
+    
+    if (!proto) {
+        // Found no specific setting for phoneToLink AddOn. Using default.
+        proto = "callto:";
+    }
 
     // replace numbers
     let body = document.body.innerHTML;

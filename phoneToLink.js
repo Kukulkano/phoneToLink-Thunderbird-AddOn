d4687b9b36fcdf37e5abf2887776b22b463df094
Volker Schmid <volker.schmid@regify.com>
2022-09-22 09:05:05 +0200

    Enhanced number detection

--- phoneToLink.js
+++ phoneToLink.js
@@ -13,7 +13,8 @@ async function makeLinks() {
     // replace numbers
     let body = document.body.innerHTML;
 
-    const r = /[^\d"':#\>](0|\+)(\d{2,})(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)/ig;
+    // HINT: Copy this to https://regex101.com for validation and testing
+    const r = /[^\d"':#\>a-z](00|0|\+)\s?(\d{2,})(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)/ig;
 
     body = body.replace(r, '<a href="'+proto+'$1$2$3$4$5$6$7">$&</a>');
 

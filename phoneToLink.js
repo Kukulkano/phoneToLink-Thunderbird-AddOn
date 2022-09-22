29be4feadce73a0410845aa9e440e4d3b555ae2b
Volker Schmid <volker.schmid@regify.com>
2022-09-22 11:22:02 +0200

    Enhanced number detection (reduce false positives in links)

--- phoneToLink.js
+++ phoneToLink.js
@@ -1,9 +1,10 @@
+var proto = undefined; // global held protocol
 
 async function makeLinks() {
     // get protocol to use
     let setting = await browser.storage.sync.get("protocol");
 
-    let proto = setting.protocol;
+    proto = setting.protocol;
     
     if (!proto) {
         // Found no specific setting for phoneToLink AddOn. Using default.
@@ -14,11 +15,22 @@ async function makeLinks() {
     let body = document.body.innerHTML;
 
     // HINT: Copy this to https://regex101.com for validation and testing
-    const r = /[^\d"':#\>a-z](00|0|\+)\s?(\d{2,})(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)/ig;
+    const r = /(\<a .*?a\>|[$\s\()]{1}(00|0|\+|&#43;|&plus;)\s?(\d{2,})(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*)(?:[ -\/\\\(\)]){0,2}(\d*))/igs;
 
-    body = body.replace(r, '<a href="'+proto+'$1$2$3$4$5$6$7">$&</a>');
+    body = body.replace(r, telReplace);
 
     document.body.innerHTML = body;
+}
+
+function telReplace(m, f1, f2, f3, f4, f5, f6, f7, f8) {
+    // console.log(m, f1, f2, f3, f4, f5, f6, f7, f8);
+
+    if (f1.substr(0, 1) === "<") {
+        // keep found links like they are
+        return m;
+    }
+    let number = f2+f3+f4+f5+f6+f7+f8;
+    return ' <a href="'+proto+number.trim()+'">'+m.trim()+'</a>';
 }
 
 makeLinks()
